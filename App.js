6e17b88cd1540d29675c4a266f05852df5b88591
marton bognar <martonbognar@sch.bme.hu>
2019-09-26 00:01:07 +0200

    Display actual data from the website

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { beverages: [{ id: '1', name: 'Dreher Bak', percentage: 7.3 }, { id: '2', name: 'Whiskey', percentage: 40 }] };
+
+    fetch('https://safedrunk.com/api/beverages/filter/a')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          beverages: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+        <FlatList
+          data={this.state.beverages}
+          renderItem={({ item }) => <Text>{item.name}, {item.percentage}</Text>}
+        />
       </View>
     );
   }
