cbda7bcea5a69052955507a183edb032bfb24a70
marton bognar <martonbognar@sch.bme.hu>
2019-09-27 00:18:17 +0200

    Get filter input from the user

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, TextInput } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { beverages: [{ id: '1', name: 'Dreher Bak', percentage: 7.3 }, { id: '2', name: 'Whiskey', percentage: 40 }] };
+    this.state = {
+      beverages: [],
+      filter: '',
+    };
 
-    fetch('https://safedrunk.com/api/beverages/filter/a')
+    this.updateList = this.updateList.bind(this);
+  }
+
+  updateList() {
+    if (this.state.filter == "") {
+      this.setState({ beverages: [] });
+      return;
+    }
+
+    fetch(`https://safedrunk.com/api/beverages/filter/${this.state.filter}`)
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
-          beverages: responseJson,
+          beverages: responseJson.map((beverage) => {
+            beverage.id = beverage.id.toString();
+            return beverage;
+          }),
         });
       })
       .catch((error) => {
@@ -21,6 +36,11 @@ export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TextInput
+          placeholder="Search for a beverage..."
+          value={this.state.filter}
+          onChangeText={(text) => this.setState({ filter: text }, this.updateList)}
+        />
         <FlatList
           data={this.state.beverages}
           renderItem={({ item }) => <Text>{item.name}, {item.percentage}</Text>}
