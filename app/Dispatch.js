ba19c5375297b7f19cb239a9f4ecc253bf907c0c
marton bognar <martonbognar@sch.bme.hu>
2020-04-04 18:49:29 +0200

    Big upgrade

--- app/Dispatch.js
+++ app/Dispatch.js
@@ -1,61 +1,79 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {Component} from 'react';
+import {View, Button} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Sessions from './Sessions';
+import Try from './scenes/Try';
 
-export default class Dispatch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: '',
-            email: '',
-            loggedIn: false,
-        };
+class Dispatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+      email: '',
+      loggedIn: false,
+    };
 
-        this.getDataFromStorage = this.getDataFromStorage.bind(this);
-    }
+    this.getDataFromStorage = this.getDataFromStorage.bind(this);
+  }
 
-    componentDidMount() {
-        this.getDataFromStorage().then(() =>
-            fetch(`https://safedrunk.com/api/personal`, {
-                headers: {
-                    'Authorization': 'Bearer ' + this.state.token,
-                }
-            })
-                .then((response) => {
-                    if (response.status === 200) {
-                        this.setState({ loggedIn: true });
-                    }
-                }));
-    }
-
-    async getDataFromStorage() {
-        try {
-            const token = await AsyncStorage.getItem('token');
-            const email = await AsyncStorage.getItem('email');
-            if (token !== null && email !== null) {
-                this.setState({ token: token, email: email });
-            }
-        } catch (e) {
-            // error reading value
+  componentDidMount() {
+    this.getDataFromStorage().then(() =>
+      fetch('https://safedrunk.com/api/personal', {
+        headers: {
+          Authorization: 'Bearer ' + this.state.token,
+        },
+      }).then(response => {
+        if (response.status === 200) {
+          //   this.setState({loggedIn: true});
         }
-    }
+      }),
+    );
+  }
 
-    render() {
-        if (this.state.loggedIn) {
-            return (<Sessions token={this.state.token} />);
-        } else {
-            return (
-                <View>
-                    <Button
-                        title="Login"
-                        onPress={() => this.props.navigation.navigate('Login')} />
-
-                    <Button
-                        title="Try it out"
-                        onPress={() => this.props.navigation.navigate('Try')} />
-                </View>
-            );
-        }
+  async getDataFromStorage() {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      const email = await AsyncStorage.getItem('email');
+      if (token !== null && email !== null) {
+        this.setState({token: token, email: email});
+      }
+    } catch (e) {
+      // error reading value
     }
-};
+  }
+
+  render() {
+    if (this.state.loggedIn) {
+      return <Sessions token={this.state.token} />;
+    } else {
+      return (
+        <View>
+          <Button
+            title="Login"
+            onPress={() => this.props.navigation.navigate('Login')}
+          />
+
+          <Button
+            title="Try it out"
+            onPress={() => this.props.navigation.navigate('Try')}
+          />
+        </View>
+      );
+    }
+  }
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Dispatch" component={Dispatch} />
+        <Stack.Screen name="Try" component={Try} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 /**
  * @format
  */
-
-import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+import App from './app/Dispatch';
+import {name as appName} from './app.json';
 
-import Try from './app/scenes/Try';
-import Dispatch from './app/Dispatch';
-import Settings from './app/scenes/Settings'
-import Login from './app/forms/Login';
-import QuickAdd from './app/forms/QuickAdd';
-import DefaultNewDrink from './app/forms/Default'
-
-const AppNavigator = createStackNavigator(
-    {
-        "Try": Try,
-        "My Settings": Settings,
-        "Quick add": QuickAdd,
-        "Beverage list": DefaultNewDrink,
-        "Login": Login,
-        "Dispatch": Dispatch,
-    },
-    {
-        initialRouteName: 'Dispatch',
-    },
-);
-
-const container = createAppContainer(AppNavigator);
-
-AppRegistry.registerComponent(appName, () => container);
+AppRegistry.registerComponent(appName, () => App);
